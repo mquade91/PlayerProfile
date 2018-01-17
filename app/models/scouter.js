2b73f391c4afb6b41c0a41f58bb5d0eebc68e173
Matt Quade <quadem09@gmail.com>
2018-01-17 16:19:27 +0000

    userSchema

--- app/models/scouter.js
+++ app/models/scouter.js
@@ -1,6 +1,10 @@
 var mongoose = require("mongoose");
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
+
+//require bcrypt
+var bcrypt = require('bcrypt-nodejs');
+
 // Create a new CommentSchema object
 var ScouterSchema = new Schema({
   firstName: {
@@ -11,16 +15,36 @@ var ScouterSchema = new Schema({
     type: String,
     required: true
   },
-  role: {
+  local: {
+    email: String,
+    required: true
+  },
+  local: {
+    email: String,
+    required: true
+  },
+  areaCovered: {
     type: String,
     required: true
   },
-    areaCovered: {
-    type: String,
-    required: true
-  },
+
   id: Schema.Types.ObjectId
 });
+
+
+// METHODS
+//generating a hash
+ScouterSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+//checking if the PASSWORD is valid
+ScouterSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
+
+// //CREATE the model for users and expose it to our App
+// module.exports = mongoose.model('User', userSchema);
 
 var Scouter = mongoose.model("Scouter", ScouterSchema);
 // Export the Comment model
