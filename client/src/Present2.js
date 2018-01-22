c323edc86b9e1c502f3f16ca47ae7588bb3756b5
Chas Campbell <chas@chascampbell.com>
2018-01-22 19:37:43 +0000

    setup react dev server

---
+++ client/src/Present2.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./Present.css";
+
+class Present2 extends Component {
+
+    render() {
+            return (
+                <div className="heading">
+                    <h1>Technology Used</h1>
+                </div>
+                
+        );
+}
+}
+export default Present2;
