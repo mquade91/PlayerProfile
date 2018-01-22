2bf3db8db72fd32d68bda2dd79e9a9fd14cf3993
Matt Quade <quadem09@gmail.com>
2018-01-22 23:00:14 +0000

    added route to mikes page and href in profile.ejs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //server.js
 var express = require('express');
 var app = express();
-var port = 8081;
+var port = 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash = require('connect-flash');
