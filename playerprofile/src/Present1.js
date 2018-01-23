f8f042667ff7496e91a39de6ce249976583fabf4
Chas Campbell <chas@chascampbell.com>
2018-01-23 21:36:11 +0000

    working on About

---
+++ playerprofile/src/Present1.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+ const Present1 = () => (
+    <div className="bodyIn">
+        <div className="bodyHead">
+            <h5>Security is Paramount</h5>
+        </div>
+            <div className="bodyList">
+                <ul className="list">
+                    <li>No Signup</li>
+                    <li>Staff Controlled</li>
+                    <li>Limited Access to Entry</li>
+                </ul>
+            </div>
+            <div className="bodyHead">
+                <h5>Future Development</h5>
+            </div>
+            <div className="bodyList">
+                <ul className="list">
+                    <li>Other Sports</li>
+                    <li>Colleges</li>
+                    <li>Other Fields</li>
+                </ul>
+            </div>
+    </div>
+                
+ );
+
+export default Present1;
