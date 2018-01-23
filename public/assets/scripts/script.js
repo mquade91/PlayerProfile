a75e15a15ae411cc1a1a5e45afe7122d7fab6351
Matt Quade <quadem09@gmail.com>
2018-01-23 17:03:33 +0000

    post new players

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -1,43 +1,50 @@
 /* global $*/
-
 $(document).ready(function() {
     console.log("scipt connected");
 
     $("#submit").on("click", function(event) {
-        var firstname;
-        var lastname;
-        var height;
-        var weight;
-        var dash;
-        var school;
-        var positionrating;
-        var overallrank;
-        var comments;
-        var listData = [];
+        event.preventDefault();
 
-        //Save the data
-        firstname = $("#firstname").val();
-        lastname = $("#lastname").val();
-        height = $("#height").val();
-        weight = $("weight").val();
-        dash = $("#dash").val();
-        school = $("#school").val();
-        positionrating = $("#positionrating").val();
-        overallrank = $("#overallrank").val();
-        comments = $("#comments").val();
-        listData = [
-            firstname,
-            lastname,
-            height,
-            weight,
-            dash,
-            school,
-            positionrating,
-            overallrank,
-            comments
-        ];
-        console.log(listData);
+        var newPlayer = {
+            firstName: $("#firstname").val().trim(),
+            lastName: $("#lastname").val().trim(),
+            height: $("#height").val().trim(),
+            weight: $("#weight").val().trim(),
+            dash: $("#dash").val().trim(),
+            school: $("#school").val().trim(),
+            position: $("#position").val().trim(),
+            // positionrating: $("#positionrating").val().trim(),
+            overallrank: $("#overallrank").val().trim(),
+            comments: $("#comments").val().trim(),
+        };
+
+        console.log("test" + newPlayer);
+
+        $.ajax({
+            url:'/newPlayer',
+            method:"POST",
+            data:newPlayer
+        }).then(function(data){
+                console.log(data);
+                if (data) {
+                    alert("New Player added to the database");
+                }
+                else {
+                    alert("Fail! Try again!");
+                }
+                // Clear the form when submitting
+                $("#firstname").val("");
+                $("#lastname").val("");
+                $("#height").val("");
+                $("#weight").val("");
+                $("#dash").val("");
+                $("#school").val("");
+                $("#position").val(""),
+                $("#positionrating").val("");
+                $("#overallrank").val("");
+                $("#comments").val("");
+
+            });
+
     });
-
-
 });
