f8fa39fa8465b1d6b272ec0b685bb12439c8b6cd
Chas Campbell <chas@chascampbell.com>
2018-01-24 04:16:32 +0000

    working on About for presentation

---
+++ playerprofile/src/App0.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import About from "./About";
+
+const App = () => (
+    <div>
+        <About/>
+    </div>
+    );
+
+export default App;
---
+++ playerprofile/src/Present0.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+ const Present0 = () => (
+    <div className="bodyIn">
+        <div className="bodyHead0">
+            <h4>Organized Data Provides Information</h4>
+        </div>
+            
+            <div className="bodyHead">
+                <h5>Information Facilitates Good Decisions</h5>
+            </div>
+            
+    </div>
+                
+ );
+
+export default Present0;
