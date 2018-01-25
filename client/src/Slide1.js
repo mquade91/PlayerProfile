bb0ba48be72e902ad373950ee5ff7463d62636be
Chas Campbell <chas@chascampbell.com>
2018-01-25 18:31:43 +0000

    About page finished

--- client/src/Slide1.js
+++ client/src/Slide1.js
@@ -1,29 +1,17 @@
 import React from "react";
 
- const Present1 = () => (
+ const Slide1 = () => (
     <div className="bodyIn">
-        <div className="bodyHead">
-            <h5>Security is Paramount</h5>
+        <div className="bodyHead0">
+            <h4>Organized Data Provides Information</h4>
         </div>
-            <div className="bodyList">
-                <ul className="list">
-                    <li>No Signup</li>
-                    <li>Staff Controlled</li>
-                    <li>Limited Access to Entry</li>
-                </ul>
-            </div>
+            
             <div className="bodyHead">
-                <h5>Future Development</h5>
+                <h5>Information Facilitates Good Decisions</h5>
             </div>
-            <div className="bodyList">
-                <ul className="list">
-                    <li>Other Sports</li>
-                    <li>Colleges</li>
-                    <li>Other Fields</li>
-                </ul>
-            </div>
+            
     </div>
                 
  );
 
-export default Present1;
+export default Slide1;
---
+++ client/src/Slide10.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+ const Slide10 = () => (
+    <div className="bodyIn">
+        <div className="bodyHead">
+            <h5>Security is Paramount</h5>
+        </div>
+        <div className="bodyList">
+            <ul className="list">
+                <li>No Signup</li>
+                <li>Staff Controlled</li>
+                <li>Limited Access to Entry</li>
+            </ul>
+        </div>
+    </div>
+                
+ );
+
+export default Slide10;
