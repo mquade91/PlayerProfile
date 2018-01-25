2fd821647741e6486e44c40d89a0e881878ff2ca
Michael Sloan <m.j.sloan88@gmail.com>
2018-01-25 19:19:02 +0000

    Updated profile. Individual cards for players

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -49,20 +49,22 @@ $(document).ready(function() {
 
         });
 
-        function getResults() {
-            $.getJSON("/athletesInfo", function(data) {
-                console.log(data);
-
-
-
-                for (var i = 0; i < data.length; i++) {
-                    $("#allPlayers").prepend("<div class='playerCard'><h1>" + data[i].firstName + " " + data[i].lastName + "</h1><p>" + "Height: " + data[i].height + "</p></div>")
-                }
-            })
-        }
-        getResults()
 
 
 
     });
+
+    function getResults() {
+        $.getJSON("/athletesInfo", function(data) {
+            console.log(data);
+
+
+
+            for (var i = 0; i < data.length; i++) {
+                $("#allPlayers").prepend("<div  class='col-md-3'><div class='playerCard'><h1>" + data[i].firstName + " " + data[i].lastName + "</h1><p>" + "Height: " + data[i].height + " ft" + "</p><p>" + "Weight: " + data[i].weight + " lbs" + "</p><p>" + "Dash: " + data[i].fortyTime + " s" + "</p><p>" + data[i].school + "</p><p>" + "" + data[i].position + "" + "</p><p>" + data[i].comment + "</p></div></div>")
+            }
+        })
+    }
+    getResults()
+
 });
