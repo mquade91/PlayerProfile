4c4bc2ec3d090de577dd298c2b4f2eb441646e8f
Chas Campbell <chas@chascampbell.com>
2018-01-29 20:30:12 +0000

    About with slides links to Home and vice-versa

---
+++ client/src/slides/Slide300.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+ const Slide300 = () => (
+   <div className="bodyIn">
+      <img src="../assets/images/slide300.png" alt="Slide300" />
+    </div>
+                
+ );
+
+export default Slide300;
---
+++ client/src/slides/Slide600.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+ const Slide600 = () => (
+    <div className="bodyIn">
+      <img src="../assets/images/slide600.png" alt="Slide600" />
+    </div>
+                
+ );
+
+export default Slide600;
