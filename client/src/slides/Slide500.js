bcaebcd66ce868c072084f70691eaa725789c754
Chas Campbell <chas@chascampbell.com>
2018-01-29 20:30:12 +0000

    About with slides links to Home and vice-versa

---
+++ client/src/slides/Slide500.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+ const Slide500 = () => (
+    <div className="bodyIn">
+      <img src="../assets/images/slide500.png" alt="Slide500" />
+    </div>
+                
+ );
+
+export default Slide500;
